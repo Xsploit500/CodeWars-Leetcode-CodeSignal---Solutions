4ac041af6ceb3b40494011a4272cd61b7277b5f1
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2024-11-16 11:54:08 -0600

    Time: 3 ms (53.42%), Space: 16.8 MB (10.85%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.py
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.py
@@ -1,5 +1,16 @@
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+
+        for _ in range(k):
+            minimum = min(nums)
+            index = nums.index(minimum)
+            nums[index] = minimum * multiplier
+
+        return nums
+
+
+
+        
         import heapq
 
         minheap = nums[:]

65ddb01f52921e09520a793ea069e9dc44fe593a
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2024-12-16 15:05:26 -0600

    Time: 7 ms (91.1%), Space: 17.2 MB (46.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        from collections import Counter
+
+        s_dict, t_dict = Counter(s), Counter(t)
+
+        for key, value in s_dict.items():
+            if key not in t_dict or t_dict[key] < value:
+                return False
+        
+        for key, value in t_dict.items():
+            if key not in s_dict or s_dict[key] < value:
+                return False
+        
+        return True
+
+
+
+        
         return sorted(s) == sorted(t)

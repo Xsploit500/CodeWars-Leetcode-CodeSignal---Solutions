50ec2d906f01c85ccc533d8482d14ba7bdb15a3d
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2024-12-16 13:40:12 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,7 +5,8 @@ class Solution:
         while ptr1 < len(s) and ptr2 < len(t):
             if s[ptr1] == t[ptr2]:
                 ptr1 += 1
-            ptr2 += 1
+            elif s[ptr1] != t[ptr2]:
+                ptr2 += 1
         
         return ptr1 == len(s)
 

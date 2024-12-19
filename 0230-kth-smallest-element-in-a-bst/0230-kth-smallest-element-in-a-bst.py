33bca52e042037b755cbbd925b9e4908f31abc9d
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2024-12-19 14:23:47 -0600

    Time: 37 ms (6.44%), Space: 19.5 MB (99.75%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,35 +6,18 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        from collections import deque
-        import heapq
+        n = 0
+        stack = []
+        cur = root
 
-        def bfs(root):
-            if not root:
-                return []
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
 
-            queue, values = deque([root]), []
-
-            while len(queue) > 0:
-                current = queue.popleft()
-
-                values.append(current.val)
-
-                if current.left:
-                    queue.append(current.left)
-                if current.right:
-                    queue.append(current.right)
-
-            return values
-
-        minheap, traversal = [], bfs(root)
-
-        for val in traversal:
-            heapq.heappush(minheap, val)
-
-        for _ in range(k):
-            popped = heapq.heappop(minheap)
-
-        return popped
-        
+            cur = stack.pop()
+            n += 1
+            if n == k:
+                return cur.val
+            cur = cur.right
         

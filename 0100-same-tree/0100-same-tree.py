15cf3587751399b62c5641f9ca2d5c7ba2e28e40
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2024-12-19 13:39:50 -0600

    Time: 0 ms (100%), Space: 17.7 MB (9.83%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,6 +6,27 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            stack, values = [root], []
+
+            while stack:
+                current = stack.pop()
+
+                if current is None:
+                    values.append("null")
+
+                else:
+                    values.append(current.val)
+                    stack.append(current.left)
+                    stack.append(current.right)
+
+            return values
+
+        return dfs(p) == dfs(q)
+
+
+
+
         if not p and not q:
             return True
         if p and not q or not p and q:

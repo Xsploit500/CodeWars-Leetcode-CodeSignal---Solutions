f07335b84eef28e136f041b3af8b641ef996b24a
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2025-02-11 16:07:29 -0600

    Time: 0 ms (100%), Space: 17.7 MB (75.6%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,11 +1,28 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        output = []
 
-        for i in range(0, len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] == nums[j] and i != j:
-                    output.append((i, j))
+        Map = {}
 
-        return len(output)
+        for num in nums:
+            if num not in Map:
+                Map[num] = 0
+                Map[num] += 1
+            else:
+                Map[num] += 1
+
+        result = 0
+
+        for key, val in Map.items():
+            result += (val * (val - 1)) // 2
+
+        return result
+
+        # output = []
+
+        # for i in range(0, len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] == nums[j] and i != j:
+        #             output.append((i, j))
+
+        # return len(output)
         

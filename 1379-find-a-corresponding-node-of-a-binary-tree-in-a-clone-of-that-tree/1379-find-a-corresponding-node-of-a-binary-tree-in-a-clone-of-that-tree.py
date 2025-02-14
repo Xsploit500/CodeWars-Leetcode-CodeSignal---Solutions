b88a4fb8d06fe25abf2065e3c73a4bfb2c77d7fb
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2025-02-14 16:58:32 -0600

    Time: 301 ms (94.29%), Space: 26.1 MB (47.21%) - LeetHub

---
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        
+        stack = [cloned]
+        
+        while stack:
+            current = stack.pop()
+
+            if current.val == target.val:
+                return current
+            
+            if current.left:
+                stack.append(current.left)
+            if current.right:
+                stack.append(current.right)

fe9d495ce4ae95c479c6f0160b72d5f75efff0b4
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2025-03-24 22:06:31 -0500

    Time: 34 ms (33.87%), Space: 17.7 MB (72.68%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,15 +1,34 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        output = []
+        Hmap, output = {}, []
 
-        for i in nums:
-            smaller = 0
+        for num in nums:
+            if num not in Hmap:
+                Hmap[num] = 0
+            Hmap[num] += 1
 
-            for j in nums:
-                if j < i:
-                    smaller += 1
-            
-            output.append(smaller)
+        for num in nums:
+            smaller_vals = 0
+
+            for key, value in Hmap.items():
+                if key < num:
+                    smaller_vals += Hmap[key]
+            output.append(smaller_vals)
 
         return output
+
+
+        
+        # output = []
+
+        # for i in nums:
+        #     smaller = 0
+
+        #     for j in nums:
+        #         if j < i:
+        #             smaller += 1
+            
+        #     output.append(smaller)
+
+        # return output
         

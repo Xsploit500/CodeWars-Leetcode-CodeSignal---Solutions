fb2724d4ae59d74c112fefbbb9ae27e7565a5a56
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2025-04-22 21:45:18 -0500

    Time: 39 ms (95.48%), Space: 19.9 MB (30.5%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,6 +6,18 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
+
+        
         visited = set()
 
         current = head

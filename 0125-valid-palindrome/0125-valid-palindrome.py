8a7e91b2df20e678985f5c184b6ae97d8e6d5eb8
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2025-04-28 13:15:35 -0500

    Time: 4 ms (90.7%), Space: 23.6 MB (5.08%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,31 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        #Two Pointer Approach
+        chars = [i.lower() for i in s if i.isalnum()]
 
-        cleaned_s = ""
-
-        for char in s:
-            if char.isalnum():
-                cleaned_s += char.lower()
-
-        left, right = 0, len(cleaned_s) - 1
-
-        while left <= right:
-            if cleaned_s[left] != cleaned_s[right]:
-                return False
-            left += 1
-            right -= 1
-        
-        return True
-
-
-
-
-        new_s = ""
-
-        for i in s:
-            if i.isalnum():
-                new_s += i.lower()
-
-        return new_s == new_s[::-1]
-        
+        return chars == chars[::-1]

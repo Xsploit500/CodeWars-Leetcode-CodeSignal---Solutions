cf28abe07f6e8ea345cdd78de17f461cfda8a811
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2025-09-05 03:24:17 -0400

    Time: 0 ms (100%), Space: 19.8 MB (29.48%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,11 +6,32 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def validate(node, min_val, max_val):
-            if not node:
-                return True
+        # Iterative DFS
+        if not root:
+            return True
+
+        stack = [(root, float('-inf'), float('inf'))]
+
+        while stack:
+            node, min_val, max_val = stack.pop()
+
             if node.val <= min_val or node.val >= max_val:
                 return False
-            return (validate(node.left, min_val, node.val) and validate(node.right, node.val, max_val))
+            
+            if node.right:
+                stack.append((node.right, node.val, max_val))
+            
+            if node.left:
+                stack.append((node.left, min_val, node.val))
 
-        return(validate(root, float("-inf"), float("inf")))
+        return True
+
+        # # Recursive Solution
+        # def validate(node, min_val, max_val):
+        #     if not node:
+        #         return True
+        #     if node.val <= min_val or node.val >= max_val:
+        #         return False
+        #     return (validate(node.left, min_val, node.val) and validate(node.right, node.val, max_val))
+
+        # return(validate(root, float("-inf"), float("inf")))

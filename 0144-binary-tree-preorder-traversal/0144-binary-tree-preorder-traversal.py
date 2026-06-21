52c917518d984787947ecdad017cca20954a653e
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2026-06-21 18:56:37 -0500

    Time: 0 ms (100%), Space: 19.1 MB (92%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = [root]
+        result = []
+        
+        if not root:
+            return []
+
+        while stack:
+            current = stack.pop()
+            result.append(current.val)
+
+            if current.right:
+                stack.append(current.right)
+
+            if current.left:
+                stack.append(current.left)
+
+        return result

d2ab82d08eb05fb0faee1c3ad7cde22ab724300f
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2026-06-23 15:24:56 -0500

    Time: 0 ms (100%), Space: 44.5 MB (77.27%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -6,16 +6,41 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        def helper(root):
-            if not root:
-                return 0
+        if not root:
+            return 0
 
-            left_call = helper(root.left)
-            right_call = helper(root.right)
+        from collections import deque
 
-            if (root.left and not root.right) or (root.right and not root.left):
-                return 1 + max(left_call, right_call)
+        depth = 1
 
-            return 1 + min(left_call, right_call)
+        queue = deque([(root, depth)])
 
-        return helper(root)
+        while queue:
+            current, depth = queue.popleft()
+
+            if not current.left and not current.right:
+                return depth
+
+            if current.left:
+                queue.append((current.left, depth + 1))
+
+            if current.right:
+                queue.append((current.right, depth + 1))
+
+
+                
+
+
+        # def helper(root):
+        #     if not root:
+        #         return 0
+
+        #     left_call = helper(root.left)
+        #     right_call = helper(root.right)
+
+        #     if (root.left and not root.right) or (root.right and not root.left):
+        #         return 1 + max(left_call, right_call)
+
+        #     return 1 + min(left_call, right_call)
+
+        # return helper(root)

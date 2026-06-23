617073b33cd0877050c2196d5e5cd60365bbc3ea
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2026-06-23 15:40:56 -0500

    Time: 1 ms (31.22%), Space: 20.2 MB (61.43%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,8 +6,66 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # if not root:
+        #     return 0
+
+        # depth = 1
+
+        # stack = [(root, depth)]
+
+        # while stack:
+        #     current, max_depth = stack.pop()
+
+        #     if max_depth > depth:
+        #         depth = max_depth
+
+        #     if current.left:
+        #         stack.append((current.left, max_depth + 1))
+
+        #     if current.right:
+        #         stack.append((current.right, max_depth + 1))
+
+        # return depth
+
+
+
+
+
+
         if not root:
             return 0
 
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        from collections import deque
+
+        depth = 1
+
+        queue = deque([(root, 1)])
+
+        while len(queue) != 0:
+            current, max_depth = queue.popleft()
+
+            if max_depth > depth:
+                depth = max_depth
+
+            if current.left:
+                queue.append((current.left, max_depth + 1))
+
+            if current.right:
+                queue.append((current.right, max_depth + 1))
+
+        return depth
+
         
+        
+        
+        
+        # def helper(root):
+        #     if not root:
+        #         return 0
+
+        #     left_call = helper(root.left)
+        #     right_call = helper(root.right)
+
+        #     return 1 + max(left_call, right_call)
+
+        # return helper(root)

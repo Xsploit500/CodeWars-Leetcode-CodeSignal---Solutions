faf4f285112ba82f1cf9936641298d875da3343f
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2026-08-05 03:55:50 -0500

    Time: 0 ms (100%), Space: 19.3 MB (77.24%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,20 +1,64 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+        def find_min_index(nums, target):
+            low, high = 0, len(nums) - 1
 
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
-                return mid
-            if nums[left] <= nums[mid]:
-                if nums[left] <= target < nums[mid]:
+            while low <= high:
+                mid = (low + high) // 2
+
+                if nums[mid] < nums[high]:
+                    high = mid
+                elif nums[mid] > nums[high]:
+                    low = mid + 1
+                else:
+                    return mid
+
+        def binary_search(nums, target):
+            left, right = 0, len(nums) - 1
+
+            while left <= right:
+                mid = (left + right) // 2
+
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] > target:
                     right = mid - 1
                 else:
                     left = mid + 1
-            else:
-                if nums[mid] < target <= nums[right]:
-                    left = mid + 1
-                else:
-                    right = mid - 1
 
-        return -1
+            return -1
+
+        min_index = find_min_index(nums, target)
+        left_half = binary_search(nums[0:min_index], target)
+        right_half = binary_search(nums[min_index:len(nums)], target)
+
+        if left_half != -1:
+            return left_half
+        elif right_half != -1:
+            return min_index + right_half
+        else:
+            return -1
+
+
+
+
+
+    # def search(self, nums: List[int], target: int) -> int:
+    #     left, right = 0, len(nums) - 1
+
+    #     while left <= right:
+    #         mid = (left + right) // 2
+    #         if nums[mid] == target:
+    #             return mid
+    #         if nums[left] <= nums[mid]:
+    #             if nums[left] <= target < nums[mid]:
+    #                 right = mid - 1
+    #             else:
+    #                 left = mid + 1
+    #         else:
+    #             if nums[mid] < target <= nums[right]:
+    #                 left = mid + 1
+    #             else:
+    #                 right = mid - 1
+
+    #     return -1

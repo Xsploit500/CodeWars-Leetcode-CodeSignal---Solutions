60ca8de685654af553081fd2bd82f3186a2329ff
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2026-09-01 08:35:00 -0500

    Time: 7 ms (52.14%), Space: 22.7 MB (94.32%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,17 +2,34 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = {}
         for num in nums:
-            count[num] = count.get(num, 0) + 1
+            if num not in count:
+                count[num] = 0
+            count[num] += 1
+        
+        count = sorted(count.items(), key=lambda x: x[1], reverse=True)
 
-        buckets = [[] for _ in range(len(nums) + 1)]
-        for val, freq in count.items():
-            buckets[freq].append(val)
+        output = []
 
-        result = []
-        for freq in range(len(buckets) - 1, 0, -1):
-            for val in buckets[freq]:
-                result.append(val)
-                if len(result) == k:
-                    return result
+        for i in range(k):
+            output.append(count[i][0])
 
-        return result        
+        return output
+
+        
+        
+        # count = {}
+        # for num in nums:
+        #     count[num] = count.get(num, 0) + 1
+
+        # buckets = [[] for _ in range(len(nums) + 1)]
+        # for val, freq in count.items():
+        #     buckets[freq].append(val)
+
+        # result = []
+        # for freq in range(len(buckets) - 1, 0, -1):
+        #     for val in buckets[freq]:
+        #         result.append(val)
+        #         if len(result) == k:
+        #             return result
+
+        # return result        

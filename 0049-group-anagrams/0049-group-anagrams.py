563f3bdfd6324039cb47b453bd622daa7f55e841
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2026-09-01 08:14:22 -0500

    Time: 9 ms (89.48%), Space: 22.7 MB (36.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,19 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groups = {}
-        for string in strs:
-            key = "".join(sorted(string))
-            if key not in groups:
-                groups[key] = []
-            groups[key].append(string)
+
+        for s in strs:
+            key = tuple(sorted(s))
+            groups.setdefault(key, []).append(s)
         return list(groups.values())
+
+        
+        
+        # groups = {}
+        # for string in strs:
+        #     key = "".join(sorted(string))
+        #     if key not in groups:
+        #         groups[key] = []
+        #     groups[key].append(string)
+        # return list(groups.values())
         

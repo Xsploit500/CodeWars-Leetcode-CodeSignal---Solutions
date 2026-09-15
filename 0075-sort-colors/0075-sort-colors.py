aa49e2ff12bd94e639ec28d87da14da26458de0e
Wisdom Ariagbofo <119155222+Xsploit500@users.noreply.github.com>
2026-09-15 08:33:56 -0500

    Time: 2 ms (13.57%), Space: 19.3 MB (23.88%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,15 +3,29 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        low, i, high = 0, 0, len(nums) - 1
 
-        while i <= high:
-            if nums[i] == 0:
-                nums[low], nums[i] = nums[i], nums[low]
-                low += 1
+        count = {}
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        i = 0
+        for key in (0, 1, 2):
+            for _ in range(count.get(key, 0)):
+                nums[i] = key
                 i += 1
-            elif nums[i] == 2:
-                nums[i], nums[high] = nums[high], nums[i]
-                high -= 1
-            else:
-                i += 1
+
+
+
+        # low, i, high = 0, 0, len(nums) - 1
+
+        # while i <= high:
+        #     if nums[i] == 0:
+        #         nums[low], nums[i] = nums[i], nums[low]
+        #         low += 1
+        #         i += 1
+        #     elif nums[i] == 2:
+        #         nums[i], nums[high] = nums[high], nums[i]
+        #         high -= 1
+        #     else:
+        #         i += 1
